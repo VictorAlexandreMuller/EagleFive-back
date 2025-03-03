4fccbca06594d9931cdc96b51e48f10b4f0bf4ba
VictorAlexandreMuller <victor.alexandre.mu@gmail.com>
2025-03-02 23:06:34 -0300

    .

---
+++ src/main/java/eaglefiveback/eaglefiveback/EaglefivebackApplication.java
@@ -0,0 +1,13 @@
+package eaglefiveback.eaglefiveback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaglefivebackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EaglefivebackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eaglefiveback/eaglefiveback/EaglefivebackApplicationTests.java
@@ -0,0 +1,13 @@
+package eaglefiveback.eaglefiveback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaglefivebackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
